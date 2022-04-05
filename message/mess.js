f88bfd2607e63225b713d5dba21bd7f42161ac35
Hildan <Komengyamaha11@gmail.com>
2022-04-05 14:30:10 +0800

    HildanBotV1

---
+++ message/mess.js
@@ -0,0 +1,20 @@
+module.exports.mess = {
+	        regist: '─ 「 *NOT REGISTERED* 」 ─\n\n*Kata Hildan*\n_Kamu Belum Mendaftar Di Bot Nya Dek, Ketik ( .daftar ) Untuk Di Ruqyah_',
+		    dewasa: '─ 「 *WARNING*  」 ─\n\n*Kata Hildan*\n_dilarang untuk anak di bawah umur, jika Anda sudah dewasa, silahkan verifikasi terlebih dahulu, ketik .saya18_',
+		    waitregist: '*Kata Hildan*\n_Tunggu Sebentar Bro_',
+	        wait: '_Bentarr..._',
+			success: '_Done, don t forget to subscribe_ : https://youtube.com/channel/UCB157jomCne961WzYHpG4gg',
+			wrongFormat: '_Format error, try again!_',
+			error: {
+				api: '_Error, apikey is not found!_',
+				stick: '_Error, just try to send a picture!_',
+				Iv: '_Error, try to give the correct link!_'
+			},
+			only: {
+				group: '_Can only be used in groups!_',
+				admin: '_Can only be used by admin!_',
+				premium: '_Can only be used by premium users!_',
+				owner: '_Can only be used by owner!_',
+				Badmin: '_Can only be used if the bot is an admin!_',
+			}
+		}
